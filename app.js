f329c42c2334afb6b47a8776a2a9fd09a48b63fe
Charlie Garnaas <cgarnaas555@gmail.com>
2017-12-09 20:57:15 -0600

    d3

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// requires express
+var express = require('express');
+//requires path
+var path = require('path');
+//shorthand express varibale
+var app = express();
+// uses static files
+var port = process.env.PORT||3555;
+
+app.use(express.static('public'));
+// /* gets all follows the path to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+// Tells the sever to listen for action on port 3004
+app.listen(port, function() {
+  console.log('Welcome to CharGar Visit us as at localhost:3555!!!');
+});
